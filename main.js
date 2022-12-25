b2420b725ee7202fa9f9c34b3435917c972c645a
Muhammedfuad Bashar <115805335+PrimeFord@users.noreply.github.com>
2022-12-25 21:04:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,6 +25,30 @@ const QuoteArray = [
     author: "Obito Uchiha",
   },
   {
+    Quote: 'God save me from my friends. I can protect myself from my enemies.',
+    Author: 'Claude Louis Hector De Villars'
+},
+{
+    Quote: 'Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we have got',
+    Author: 'life'
+},
+{
+    Quote: 'The price of anything is the amount of life you exchange for it.',
+    Author: 'David Thoreau'
+},
+{
+    Quote: 'Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance.',
+    Author: 'Charles Lindbergh'
+},
+{
+    Quote: 'A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.',
+    Author: 'Tyne Daly'
+},
+{
+    Quote: 'Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all',
+    Author: 'John Kenneth Galbraith'
+},
+  {
     quote: "Love is not necessary, Power is the only true necessity",
     author: "Madara Uchiha",
   },
